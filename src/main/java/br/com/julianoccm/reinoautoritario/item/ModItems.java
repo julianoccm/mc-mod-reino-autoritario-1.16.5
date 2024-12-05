11cd4491d7f52a8894111c2e72d6ef082ce4c08e
julianoccm <julianocolerecmoreira@gmail.com>
2024-12-05 01:25:46 -0300

    feat: Ghost badge

--- src/main/java/br/com/julianoccm/reinoautoritario/item/ModItems.java
+++ src/main/java/br/com/julianoccm/reinoautoritario/item/ModItems.java
@@ -35,6 +35,9 @@ public class ModItems {
     public static final RegistryObject<Item> INSIGNIA_INSETO = ITEMS.register("pixelmon/insignia_inseto",
             () -> new Item(new Item.Properties().tab(ModItemGroup.REINO_AUTORITARIO_PIXELMON).stacksTo(64)));
 
+    public static final RegistryObject<Item> INSIGNIA_FANSTASMA = ITEMS.register("pixelmon/insignia_fantasma",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.REINO_AUTORITARIO_PIXELMON).stacksTo(64)));
+
     // Mobs Eggs
     public static final RegistryObject<ModSpawnEggItem> REM_SPAWN_EGG = ITEMS.register("mobs/rem_spawn_egg",
             () -> new ModSpawnEggItem(ModEntityTypes.REM_ENTITY, new Item.Properties().tab(ModItemGroup.REINO_AUTORITARIO_MOBS)
