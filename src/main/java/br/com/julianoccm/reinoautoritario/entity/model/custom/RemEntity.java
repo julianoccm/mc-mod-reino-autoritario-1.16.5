6af41c900a830d7a8c927509fb843fa8d936a7ec
julianoccm <julianocolerecmoreira@gmail.com>
2024-12-04 22:08:32 -0300

    feat: Look at player

--- src/main/java/br/com/julianoccm/reinoautoritario/entity/model/custom/RemEntity.java
+++ src/main/java/br/com/julianoccm/reinoautoritario/entity/model/custom/RemEntity.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 public class RemEntity extends MobEntity {
@@ -18,5 +20,7 @@ public class RemEntity extends MobEntity {
     @Override
     protected void registerGoals() {
         super.registerGoals();
+        this.goalSelector.addGoal(0, new LookAtGoal(this, PlayerEntity.class, 8.0f));
+
     }
 }
