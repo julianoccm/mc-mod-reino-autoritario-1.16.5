910e89ca19b88d7b98cced3afa1ccbc87d3feb8b
julianoccm <julianocolerecmoreira@gmail.com>
2024-12-05 00:25:26 -0300

    feat: Created NPCs renderers

---
+++ src/main/java/br/com/julianoccm/reinoautoritario/entity/renderer/MistyRenderer.java
@@ -0,0 +1,22 @@
+package br.com.julianoccm.reinoautoritario.entity.renderer;
+
+import br.com.julianoccm.reinoautoritario.ReinoAutoritarioMod;
+import br.com.julianoccm.reinoautoritario.entity.custom.RamEntity;
+import br.com.julianoccm.reinoautoritario.entity.model.NPCModel;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.util.ResourceLocation;
+
+public class MistyRenderer extends MobRenderer<RamEntity, NPCModel<RamEntity>> {
+
+    public static final ResourceLocation TEXTURE = new ResourceLocation(ReinoAutoritarioMod.MOD_ID, "textures/entity/misty.png");
+
+    public MistyRenderer(EntityRendererManager entityRendererManager) {
+        super(entityRendererManager, new NPCModel<>(), 0.7f);
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(RamEntity npcMobEntity) {
+        return TEXTURE;
+    }
+}
